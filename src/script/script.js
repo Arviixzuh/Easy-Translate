0d91241a181ac613fdce72a7ee3a54e3eeae01a0
Arviixzuh <victorgabriel725456@gmail.com>
2022-12-30 23:37:48 -0400

    Update script.js

--- src/script/script.js
+++ src/script/script.js
@@ -28,13 +28,13 @@ const selectSecond = document.querySelector(".second")
 const translate = document.querySelector(".translate")
 const fromText = document.querySelector(".fromText")
 const toText = document.querySelector(".toText")
-const change =  document.getElementById("change")
+const change = document.getElementById("change")
 const reades = document.querySelectorAll(".read")
 const listen = document.querySelector(".listen")
 const language1 = "en-GB"
 const language2 = "es-ES"
 
-for(const i in languages){
+for (const i in languages) {
     const key = Object.keys(languages[i]).toString()
     const value = Object.values(languages[i]).toString()
     selectFirst.innerHTML += `<option value=${key}>${value}</option>`
@@ -44,39 +44,47 @@ for(const i in languages){
 selectFirst.value = language1
 selectSecond.value = language2
 
-change.addEventListener("click",_=>{
+change.addEventListener("click", _ => {
     const selectFirstValue = selectFirst.value
     selectFirst.value = selectSecond.value
     selectSecond.value = selectFirstValue
 
-    if(!toText.value) return
+    if (!toText.value) return
     const fromTextValue = fromText.value
     fromText.value = toText.value
     toText.value = fromTextValue
 })
 
-translate.addEventListener("click",async _=>{
-    if(!fromText.value) return
+translate.addEventListener("click", async _ => {
+    if (!fromText.value) return
+    toText.setAttribute('placeholder', 'Traduciendo...');
     const res = await fetch(`https://api.mymemory.translated.net/get?q=${fromText.value}&langpair=${selectFirst.value}|${selectSecond.value}`)
     const data = await res.json()
     toText.value = data.responseData.translatedText
 })
 
+fromText.addEventListener('keyup', () => {
+    if (!fromText.value) {
+        toText.value = "";
+        toText.setAttribute('placeholder', 'Traducción');
+    }
+})
+
 var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
 const recognition = new SpeechRecognition()
 
-reades.forEach((read,index)=>{
-    read.addEventListener("click",_=>{
+reades.forEach((read, index) => {
+    read.addEventListener("click", _ => {
         const textToRead = index == 0 ? fromText.value : toText.value
-        if(!textToRead) return
+        if (!textToRead) return
         speechSynthesis.speak(new SpeechSynthesisUtterance(textToRead))
     })
 });
 
-recognition.onresult = (event)=>{
+recognition.onresult = (event) => {
     fromText.value = event.results[0][0].transcript
 }
 
-listen.addEventListener("click",_=>{
+listen.addEventListener("click", _ => {
     recognition.start()
 })

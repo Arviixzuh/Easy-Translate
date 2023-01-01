19a81545ddc754468ef1317e6ddb6ac779af5df0
Arviixzuh <victorgabriel725456@gmail.com>
2023-01-01 03:34:35 -0400

    Update script.js

--- src/script/script.js
+++ src/script/script.js
@@ -31,8 +31,9 @@ const toText = document.querySelector(".toText")
 const change = document.getElementById("change")
 const reades = document.querySelectorAll(".read")
 const listen = document.querySelector(".listen")
-const language1 = "en-GB"
-const language2 = "es-ES"
+const microphone = document.querySelector(".bx-microphone")
+const language1 = "es-ES"
+const language2 = "en-GB"
 
 for (const i in languages) {
     const key = Object.keys(languages[i]).toString()
@@ -44,7 +45,7 @@ for (const i in languages) {
 selectFirst.value = language1
 selectSecond.value = language2
 
-change.addEventListener("click", _ => {
+change.addEventListener("click", () => {
     const selectFirstValue = selectFirst.value
     selectFirst.value = selectSecond.value
     selectSecond.value = selectFirstValue
@@ -55,13 +56,14 @@ change.addEventListener("click", _ => {
     toText.value = fromTextValue
 })
 
-translate.addEventListener("click", async _ => {
+translate.addEventListener("click", async () => {
     if (!fromText.value) return
     toText.setAttribute('placeholder', 'Traduciendo...');
     const res = await fetch(`https://api.mymemory.translated.net/get?q=${fromText.value}&langpair=${selectFirst.value}|${selectSecond.value}`)
     const data = await res.json()
     toText.value = data.responseData.translatedText
 })
+
 
 fromText.addEventListener('keyup', () => {
     if (!fromText.value) {
@@ -70,21 +72,29 @@ fromText.addEventListener('keyup', () => {
     }
 })
 
-var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
-const recognition = new SpeechRecognition()
-
 reades.forEach((read, index) => {
-    read.addEventListener("click", _ => {
+    read.addEventListener("click", () => {
         const textToRead = index == 0 ? fromText.value : toText.value
         if (!textToRead) return
         speechSynthesis.speak(new SpeechSynthesisUtterance(textToRead))
     })
 });
 
-recognition.onresult = (event) => {
-    fromText.value = event.results[0][0].transcript
-}
-
-listen.addEventListener("click", _ => {
+listen.addEventListener("click", async () => {
     recognition.start()
+    microphone.classList.toggle('microfonoActivado')
 })
+
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+const recognition = new SpeechRecognition()
+recognition.lang = selectFirst.value
+
+recognition.onresult = async (event) => {
+    microphone.classList.toggle('microfonoActivado')
+    toText.setAttribute('placeholder', 'Traduciendo...');
+    fromText.value = event.results[0][0].transcript
+    const res = await fetch(`https://api.mymemory.translated.net/get?q=${fromText.value}&langpair=${selectFirst.value}|${selectSecond.value}`)
+    const data = await res.json()
+    toText.value = data.responseData.translatedText
+    speechSynthesis.speak(new SpeechSynthesisUtterance(toText.value))
+}
